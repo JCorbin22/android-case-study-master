1c12df512ad7f4b0fd79adae8a01ef3d42311455
Jake <jonjcorbin@gmail.com>
2021-07-04 15:31:53 -0500

    Adapting deals list page UI to comps

--- DESIGN.md
+++ DESIGN.md
@@ -18,13 +18,13 @@ TO-DO List:
 2. Create View/ViewModel - Deal Details **DONE**
 3. Add Retrofit to project and create API service for both list and details **DONE**
 4. Use LiveData and retrieve the product list w/ Progress bar   **DONE**
-5. Optional: Theme the progress bar as Target (maybe use animations)
-6. Update RCV to have dynamic image, and get from Model (might have to use a network image view to load a URL)
+6. Update RCV to have dynamic image, and get from Model (might have to use a network image view to load a URL) **DONE**
 7. Format UI according to the comps
 8. Handle taps on specific RecyclerView Items to go to Details **DONE**
 9. Get details loaded from service **DONE**
 10: Optional: Carts/Lists? Could use Room to cache the list items
 11: Optional: Cache Service Responses
+5. Optional: Theme the progress bar as Target (maybe use animations)
 
 - Get a credit card validator algorithm going for the validator
 - Get the credit card transactions "working" (We may need some kind of reactive framework to enable/disable submit based on entry)
--- app/src/main/java/com/target/targetcasestudy/view/DealItemAdapter.kt
+++ app/src/main/java/com/target/targetcasestudy/view/DealItemAdapter.kt
@@ -11,8 +11,11 @@ import com.squareup.picasso.Picasso
 import com.target.targetcasestudy.R
 import com.target.targetcasestudy.interfaces.IDealClickDelegate
 import com.target.targetcasestudy.model.Product
+import java.util.*
 
-class DealItemAdapter(private val products: List<Product>) : RecyclerView.Adapter<DealItemViewHolder>() {
+class DealItemAdapter(
+  private val products: List<Product>
+  ) : RecyclerView.Adapter<DealItemViewHolder>() {
 
   lateinit var dealClickDelegate: IDealClickDelegate
 
@@ -28,22 +31,40 @@ class DealItemAdapter(private val products: List<Product>) : RecyclerView.Adapte
 
   override fun onBindViewHolder(viewHolder: DealItemViewHolder, position: Int) {
     val item = products[position]
+
+    // Product details
     viewHolder.productTitleTextView.text = item.title
-    viewHolder.productSalePriceTextView.text = item.salePrice?.displayString ?: item.regularPrice.displayString
+    viewHolder.productSalePriceTextView.text = item.salePrice?.displayString
+      ?: item.regularPrice.displayString
+    viewHolder.productAisleTextView.text = item.aisle.toUpperCase(Locale.US)
+
+    // Load image from URL, set content description for accessibility
+    viewHolder.productImageView.contentDescription = "Image of product ${item.title}"
     if(item.imageUrl != null) {
       Picasso.get().load(item.imageUrl)
         .placeholder(R.drawable.ic_launcher_foreground)
         .into(viewHolder.productImageView)
     }
+
+    // Handle taps on the entire row object (would change if actions were expected on "ship" text)
     viewHolder.dealItemLayout.setOnClickListener {
       dealClickDelegate.dealClicked(item.id)
+    }
+
+    // Do not display the divider line if it is the last position in the list
+    if(position == itemCount - 1) {
+      viewHolder.productListDivider.visibility = View.GONE
+    } else {
+      viewHolder.productListDivider.visibility = View.VISIBLE
     }
   }
 }
 
 class DealItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-  var dealItemLayout: LinearLayoutCompat = itemView.findViewById(R.id.deal_list_item)
-  var productTitleTextView: TextView = itemView.findViewById(R.id.deal_list_item_title)
-  var productSalePriceTextView: TextView = itemView.findViewById(R.id.deal_list_item_price)
+  var dealItemLayout: LinearLayoutCompat = itemView.findViewById(R.id.deal_list_item_layout)
+  var productTitleTextView: TextView = itemView.findViewById(R.id.deal_list_title_tv)
+  var productSalePriceTextView: TextView = itemView.findViewById(R.id.deal_list_price_tv)
+  var productAisleTextView: TextView = itemView.findViewById(R.id.deal_list_aisle_tv)
   var productImageView: ImageView = itemView.findViewById(R.id.deal_list_item_image_view)
+  var productListDivider: View = itemView.findViewById(R.id.deal_list_item_divider_view)
 }
