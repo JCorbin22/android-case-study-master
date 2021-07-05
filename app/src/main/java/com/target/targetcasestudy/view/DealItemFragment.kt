3c8a2eddb68c0914f6724e95d2739f99fc41c535
Jake <jonjcorbin@gmail.com>
2021-07-05 12:44:16 -0500

    Adapted details page to match comps

--- app/src/main/java/com/target/targetcasestudy/view/DealItemFragment.kt
+++ app/src/main/java/com/target/targetcasestudy/view/DealItemFragment.kt
@@ -2,6 +2,7 @@ package com.target.targetcasestudy.view
 
 import android.app.Dialog
 import android.graphics.Color
+import android.graphics.Paint
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -24,7 +25,10 @@ class DealItemFragment : DialogFragment() {
 
   lateinit var progressBar: ProgressBar
   lateinit var titleTextView: TextView
-  lateinit var priceTextView: TextView
+  lateinit var salePriceTextView: TextView
+  lateinit var regPriceTextView: TextView
+  lateinit var regPriceLabel: TextView
+  lateinit var descriptionTextView: TextView
   lateinit var productImageView: ImageView
 
   override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -55,8 +59,11 @@ class DealItemFragment : DialogFragment() {
     // Inflate the layout for this fragment
     val view = inflater.inflate(R.layout.fragment_deal_item, container, false)
     progressBar = view.findViewById(R.id.details_progress_bar)
-    titleTextView = view.findViewById(R.id.deal_list_item_title)
-    priceTextView = view.findViewById(R.id.deal_list_item_price)
+    titleTextView = view.findViewById(R.id.product_detail_title_tv)
+    salePriceTextView = view.findViewById(R.id.product_detail_sale_price_tv)
+    regPriceTextView = view.findViewById(R.id.product_detail_reg_price_tv)
+    regPriceLabel = view.findViewById(R.id.reg_price_label)
+    descriptionTextView = view.findViewById(R.id.product_detail_desc_tv)
     productImageView = view.findViewById(R.id.deal_list_item_image_view)
     return view
   }
@@ -72,7 +79,19 @@ class DealItemFragment : DialogFragment() {
   private fun updateProductDetails(product: Product?) {
     if(product != null) {
       titleTextView.text = product.title
-      priceTextView.text = product.salePrice?.displayString ?: product.regularPrice.displayString
+      descriptionTextView.text = product.description
+      if(product.salePrice != null) {
+        salePriceTextView.text = product.salePrice.displayString
+        regPriceTextView.text = product.regularPrice.displayString
+        regPriceTextView.paintFlags = regPriceTextView.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+        regPriceTextView.visibility = View.VISIBLE
+        regPriceLabel.visibility = View.VISIBLE
+      } else {
+        salePriceTextView.text = product.regularPrice.displayString
+        regPriceLabel.visibility = View.GONE
+        regPriceTextView.visibility = View.GONE
+      }
+
       if(product.imageUrl != null) {
         Picasso.get().load(product.imageUrl)
           .placeholder(R.drawable.ic_launcher_foreground)
